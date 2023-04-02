71ed62271824356a0cf4c332ee59f4ef2b26755f
daved <daved@audiblelogic.com>
2023-04-02 17:15:52 -0600

    comps.py WIP

--- pinon/comps.py
+++ pinon/comps.py
@@ -6,8 +6,12 @@ import names as pn_cols
 class Comps:
     def __init__(self, config):
         self.config = config
-        self.comp_ratios = None
         self.all_ratios = None
+        self.all_ks = None
+        self.peer_ks = None
+        # m = self.config.companies.index.isin(config.companies.loc['AXP'][pn_cols.PEER_LIST])
+        # m = self.config.companies.loc[self.companies.index in self.config.companies.loc['AXP'][pn_cols.PEER_LIST]]
+        # self.sum_peer_weights = self.config.companies[pn_cols.PEER_WEIGHT].sum()
 
     def run_ratios(self):
         self.all_ratios = None
@@ -24,25 +28,48 @@ class Comps:
         if self.all_ratios is None:
             self.run_ratios()
 
+        self.all_ks = None
         for target_ticker, company in self.config.companies.iterrows():
             if company[pn_cols.EVALUATE]:
                 peer_list = company[pn_cols.PEER_LIST]
                 target_ratios = self.all_ratios.loc[target_ticker]
-                # ndx = pd.MultiIndex.from_product([peer_list, target_ratios.index], names=[pn_cols.TICKER, pn_cols.REPORT_DATE])
-                # all_ks = pd.DataFrame(index=ndx, columns=[pn_cols.TICKER, pn_cols. pn_cols.K])
-                # all_ks.sort_index()
-                all_ks = None
                 for peer_ticker in peer_list:
                     peer_ratios = self.all_ratios.loc[peer_ticker]
-                    k_pe = pd.DataFrame(columns=[pn_cols.K_QTR_PE, pn_cols.K_TTM_QTR_PE, pn_cols.TICKER])
+                    k_pe = pd.DataFrame(columns=[pn_cols.TARGET_TICKER, pn_cols.PEER_TICKER, pn_cols.K_QTR_PE, pn_cols.K_TTM_QTR_PE])
                     k_pe.loc[:, pn_cols.K_TTM_QTR_PE] = target_ratios[pn_cols.TTM_QTR_PE_RATIO] / peer_ratios[pn_cols.TTM_QTR_PE_RATIO]
                     k_pe.loc[:, pn_cols.K_QTR_PE] = target_ratios[pn_cols.QTR_PE_RATIO] / peer_ratios[pn_cols.QTR_PE_RATIO]
-                    k_pe[pn_cols.TICKER] = peer_ticker
-                    all_ks = k_pe if all_ks is None else pd.concat([all_ks, k_pe])
+                    k_pe[pn_cols.TARGET_TICKER] = target_ticker
+                    k_pe[pn_cols.PEER_TICKER] = peer_ticker
+                    self.all_ks = k_pe if self.all_ks is None else pd.concat([self.all_ks, k_pe])
 
-                all_ks.reset_index(inplace=True)
-                all_ks.set_index([pn_cols.TICKER, pn_cols.REPORT_DATE], inplace=True)
-                print('Here')
+        self.all_ks.reset_index(inplace=True)
+        self.all_ks.set_index([pn_cols.TARGET_TICKER, pn_cols.PEER_TICKER, pn_cols.REPORT_DATE], inplace=True)
+
+    def run_peer_ks(self):
+        if self.all_ks is None:
+            self.run_variations()
+
+        for target_ticker, target_ks in self.all_ks.groupby(level=pn_cols.TARGET_TICKER):
+            m = self.config.companies.index.isin(self.config.companies.loc[target_ticker][pn_cols.PEER_LIST])
+            sum_peer_weights = self.config.companies.loc[m][pn_cols.PEER_WEIGHT].sum()
+            for peer_ticker, peer_ks in target_ks.groupby(level=pn_cols.PEER_TICKER):
+                peer_k = pd.DataFrame(columns=[pn_cols.TARGET_TICKER, pn_cols.PEER_TICKER, pn_cols.K_PEER_QTR_PE, pn_cols.K_PEER_TTM_QTR_PE, pn_cols.K_PEER_WEIGHT])
+                k_peer_qtr_pe = peer_ks[pn_cols.K_QTR_PE].mean()
+                k_peer_ttm_qtr_pe = peer_ks[pn_cols.K_TTM_QTR_PE].mean()
+
+                k_peer_weight = self.config.companies.loc[peer_ticker][pn_cols.PEER_WEIGHT] / sum_peer_weights
+                peer_k.loc[len(peer_k.index)] = [target_ticker, peer_ticker, k_peer_qtr_pe, k_peer_ttm_qtr_pe, k_peer_weight]
+
+                self.peer_ks = peer_k if self.peer_ks is None else pd.concat([self.peer_ks, peer_k])
+        self.peer_ks.reset_index(inplace=True, drop=True)
+        self.peer_ks.set_index([pn_cols.TARGET_TICKER, pn_cols.PEER_TICKER], inplace=True)
+        print('Here')
+    def run_target_ks(self):
+        if self.peer_ks is None:
+            self.run_peer_ks()
+
+        for target_ticker, peer_ks in self.peer_ks.groupby(level=pn_cols.TARGET_TICKER):
+            print(target_ticker)
 
 
 
