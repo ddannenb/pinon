3d16b33c8e42701453f6bc608fe6c4d0fe43f956
daved <daved@audiblelogic.com>
2023-04-16 10:28:50 -0600

    Refactor. Add num years limits.

--- pinon/comps.py
+++ pinon/comps.py
@@ -6,37 +6,25 @@ import names as pn_cols
 class Comps:
     def __init__(self, config):
         self.config = config
-        self.all_ratios = None
         self.all_ks = None
         self.peer_ks = None
         self.target_ks = None
-
-    def run_ratios(self):
-        self.all_ratios = None
-        for ticker in self.config.companies.index:
-            m = pn.Multiples(ticker, self.config)
-            ratios = m.run_multiples()
-            ratios[pn_cols.TICKER] = ticker
-            self.all_ratios = ratios if self.all_ratios is None else pd.concat([self.all_ratios, ratios])
-
-        self.all_ratios.reset_index(inplace=True)
-        self.all_ratios.set_index([pn_cols.TICKER, pn_cols.REPORT_DATE], inplace=True)
-
+        self.multiples = None
+        self.m = pn.Multiples(config)
     def run_variations(self):
-        self.all_ks = None
-        if self.all_ratios is None:
-            self.run_ratios()
+        if self.multiples is None:
+            self.multiples = self.m.run_multiples()
 
         self.all_ks = None
         for target_ticker, company in self.config.companies.iterrows():
             if company[pn_cols.EVALUATE]:
                 peer_list = company[pn_cols.PEER_LIST]
-                target_ratios = self.all_ratios.loc[target_ticker]
+                target_multiples = self.multiples.loc[target_ticker]
                 for peer_ticker in peer_list:
-                    peer_ratios = self.all_ratios.loc[peer_ticker]
-                    k_pe = pd.DataFrame(columns=[pn_cols.TARGET_TICKER, pn_cols.PEER_TICKER, pn_cols.K_QTR_PE, pn_cols.K_TTM_QTR_PE])
-                    k_pe.loc[:, pn_cols.K_TTM_QTR_PE] = target_ratios[pn_cols.TTM_QTR_PE_RATIO] / peer_ratios[pn_cols.TTM_QTR_PE_RATIO]
-                    k_pe.loc[:, pn_cols.K_QTR_PE] = target_ratios[pn_cols.QTR_PE_RATIO] / peer_ratios[pn_cols.QTR_PE_RATIO]
+                    peer_multiples = self.multiples.loc[peer_ticker]
+                    k_pe = pd.DataFrame(columns=[pn_cols.TARGET_TICKER, pn_cols.PEER_TICKER, pn_cols.K_QTR_PE, pn_cols.K_TTM_PE])
+                    k_pe.loc[:, pn_cols.K_TTM_PE] = target_multiples[pn_cols.TTM_PE_RATIO] / peer_multiples[pn_cols.TTM_PE_RATIO]
+                    k_pe.loc[:, pn_cols.K_QTR_PE] = target_multiples[pn_cols.QTR_PE_RATIO] / peer_multiples[pn_cols.QTR_PE_RATIO]
                     k_pe[pn_cols.TARGET_TICKER] = target_ticker
                     k_pe[pn_cols.PEER_TICKER] = peer_ticker
                     self.all_ks = k_pe if self.all_ks is None else pd.concat([self.all_ks, k_pe])
@@ -53,9 +41,9 @@ class Comps:
             m = self.config.companies.index.isin(self.config.companies.loc[target_ticker][pn_cols.PEER_LIST])
             sum_peer_weights = self.config.companies.loc[m][pn_cols.PEER_WEIGHT].sum()
             for peer_ticker, peer_ks in target_ks.groupby(level=pn_cols.PEER_TICKER):
-                peer_k = pd.DataFrame(columns=[pn_cols.TARGET_TICKER, pn_cols.PEER_TICKER, pn_cols.K_PEER_QTR_PE, pn_cols.K_PEER_TTM_QTR_PE, pn_cols.K_PEER_WEIGHT])
+                peer_k = pd.DataFrame(columns=[pn_cols.TARGET_TICKER, pn_cols.PEER_TICKER, pn_cols.K_PEER_QTR_PE, pn_cols.K_PEER_TTM_PE, pn_cols.K_PEER_WEIGHT])
                 k_peer_qtr_pe = peer_ks[pn_cols.K_QTR_PE].mean()
-                k_peer_ttm_qtr_pe = peer_ks[pn_cols.K_TTM_QTR_PE].mean()
+                k_peer_ttm_qtr_pe = peer_ks[pn_cols.K_TTM_PE].mean()
 
                 k_peer_weight = self.config.companies.loc[peer_ticker][pn_cols.PEER_WEIGHT] / sum_peer_weights
                 peer_k.loc[len(peer_k.index)] = [target_ticker, peer_ticker, k_peer_qtr_pe, k_peer_ttm_qtr_pe, k_peer_weight]
@@ -70,9 +58,9 @@ class Comps:
             self.run_peer_ks()
 
         for target_ticker, peer_ks in self.peer_ks.groupby(level=pn_cols.TARGET_TICKER):
-            target_k = pd.DataFrame(columns=[pn_cols.TARGET_TICKER, pn_cols.K_TARGET_QTR_PE, pn_cols.K_TARGET_TTM_QTR_PE])
+            target_k = pd.DataFrame(columns=[pn_cols.TARGET_TICKER, pn_cols.K_TARGET_QTR_PE, pn_cols.K_TARGET_TTM_PE])
             k_target_qtr_pe = (peer_ks[pn_cols.K_PEER_QTR_PE] * peer_ks[pn_cols.K_PEER_WEIGHT]).sum()
-            k_target_qtr_ttm_pe = (peer_ks[pn_cols.K_PEER_TTM_QTR_PE] * peer_ks[pn_cols.K_PEER_WEIGHT]).sum()
+            k_target_qtr_ttm_pe = (peer_ks[pn_cols.K_PEER_TTM_PE] * peer_ks[pn_cols.K_PEER_WEIGHT]).sum()
             target_k.loc[len(target_k.index)] = [target_ticker, k_target_qtr_pe, k_target_qtr_ttm_pe]
             self.target_ks = target_k if self.target_ks is None else pd.concat([self.target_ks, target_k])
         self.target_ks.reset_index(inplace=True, drop=True)
--- pinon/multiples.py
+++ pinon/multiples.py
@@ -5,30 +5,22 @@ import simfin.names as sf_cols
 import names as pn_cols
 from daily_prices import DailyPrices
 from fundamentals import Fundamentals
+
+PE_COLS = [pn_cols.QTR_EPS, pn_cols.TTM_EPS, pn_cols.QTR_PE_RATIO, pn_cols.TTM_PE_RATIO]
 class Multiples:
     def __init__(self, config):
         # self.ticker = ticker
         self.config = config
-        self.all_multiples = None
         self.multiples = None
         self.mu_multiples = None
         self.daily_prices = DailyPrices()
         self.fundamentals = Fundamentals()
-        self.breaking_report = None
-        self.breaking_report_date = None
 
     def run_multiples(self):
-        # self.all_multiples = None
         self.init_multiples()
-        for ticker in self.config.companies.index:
-            df = self.init_multiples()
-            multiples = self.calc_quarterly_pe(ticker, df)
-            # TODO additional multiple calc go here
-            multiples[pn_cols.TICKER] = ticker
-            self.all_multiples = multiples if self.all_multiples is None else pd.concat([self.all_multiples, multiples])
-
-        self.all_multiples.reset_index(inplace=True)
-        self.all_multiples.set_index([pn_cols.TICKER, pn_cols.REPORT_DATE], inplace=True)
+        self.calc_quarterly_pe()
+        # TODO additional multiple calc go here
+        return self.multiples
 
     def init_multiples(self):
         self.multiples = None
@@ -37,7 +29,11 @@ class Multiples:
             inc = self.fundamentals.get_quarterly_income_statement(ticker)
             dp = self.daily_prices.get_downsampled_prices(ticker)
             ndx = dp.index.intersection(inc.index)
-            # TODO limit results to length of financials or num_years_requested
+
+            # Limit data to number of years requested
+            past_qtrs = self.config.companies.loc[ticker, pn_cols.PAST_YEARS_REQUESTED] * 4
+            s_ndx = len(ndx) - past_qtrs if len(ndx) > past_qtrs else 0
+            ndx = ndx.take([*range(s_ndx, len(ndx))])
 
             # Breaking reports
             if ticker in self.config.breaking_reports.index:
@@ -49,8 +45,7 @@ class Multiples:
                     self.config.breaking_reports.at[(ticker, upcoming_qtr), pn_cols.BREAKING_EMPLOYED] = True
                     print(f"Adding breaking report for ticker {ticker} for report date {upcoming_qtr.strftime('%Y-%m-%d')}")
 
-            pe_cols = [pn_cols.QTR_EPS, pn_cols.TTM_QTR_EPS, pn_cols.QTR_PE_RATIO, pn_cols.TTM_QTR_PE_RATIO]
-            df = pd.DataFrame(index=ndx, columns=pe_cols)
+            df = pd.DataFrame(index=ndx, columns=PE_COLS)
             df.index.name = pn_cols.REPORT_DATE
             df[pn_cols.MU_QTR_PRICE] = dp[sf_cols.CLOSE]
             df[pn_cols.TICKER] = ticker
@@ -58,35 +53,46 @@ class Multiples:
 
         self.multiples.reset_index(inplace=True)
         self.multiples.set_index([pn_cols.TICKER, pn_cols.REPORT_DATE], inplace=True)
-        print('Here')
+        self.multiples.sort_index(inplace=True)
 
-    def calc_quarterly_pe(self, ticker, multiples):
-        inc = self.fundamentals.get_quarterly_income_statement(ticker)
-        dp = self.daily_prices.get_downsampled_prices(ticker)
-        pe_cols = [pn_cols.QTR_EPS, pn_cols.TTM_QTR_EPS, pn_cols.QTR_PE_RATIO, pn_cols.TTM_QTR_PE_RATIO]
-        pe = pd.DataFrame(index=self.multiples.loc[ticker].index, columns=pe_cols)
+    def calc_quarterly_pe(self):
+        for ticker in self.config.companies.index:
+            inc = self.fundamentals.get_quarterly_income_statement(ticker)
+            dp = self.daily_prices.get_downsampled_prices(ticker)
+            pe = pd.DataFrame(index=self.multiples.loc[ticker].index, columns=PE_COLS)
 
-        pe[pn_cols.QTR_EPS] = inc[sf_cols.NET_INCOME] / inc[sf_cols.SHARES_DILUTED]
-        br = self.config.get_breaking_report(ticker)
-        if br is not None:
-            pe.loc[(br[pn_cols.REPORT_DATE]), pn_cols.QTR_EPS] = br[pn_cols.EPS_BREAKING]
-        pe[pn_cols.TTM_QTR_EPS] = pe[pn_cols.QTR_EPS].rolling(4).sum()
-        pe[pn_cols.QTR_PE_RATIO] = dp[sf_cols.CLOSE] / pe[pn_cols.QTR_EPS] / 4
-        pe[pn_cols.TTM_QTR_PE_RATIO] = dp[sf_cols.CLOSE] / pe[pn_cols.TTM_QTR_EPS]
+            pe[pn_cols.QTR_EPS] = inc[sf_cols.NET_INCOME] / inc[sf_cols.SHARES_DILUTED]
+            br = self.config.get_breaking_report(ticker)
+            if br is not None:
+                pe.loc[(br[pn_cols.REPORT_DATE]), pn_cols.QTR_EPS] = br[pn_cols.EPS_BREAKING]
+            pe[pn_cols.TTM_EPS] = pe[pn_cols.QTR_EPS].rolling(4).sum()
+            pe[pn_cols.QTR_PE_RATIO] = dp[sf_cols.CLOSE] / pe[pn_cols.QTR_EPS] / 4
+            pe[pn_cols.TTM_PE_RATIO] = dp[sf_cols.CLOSE] / pe[pn_cols.TTM_EPS]
 
-        self.multiples.loc[(ticker, ), pe_cols] = pe.values
-        print('HERE')
+            self.multiples.loc[(ticker, ), PE_COLS] = pe.values
+
     def run_mu_multiples(self):
+        self.mu_multiples = None
+
         if self.multiples is None:
             self.run_multiples()
 
-        self.mu_multiples = pd.DataFrame(columns=[pn_cols.MU_QTR_PE_RATIO, pn_cols.MU_TTM_QTR_PE_RATIO], index=['0 year', '1 year','3 year', '5 year'])
-        self.mu_multiples.index.name = pn_cols.MU_NUM_YEARS
+        for ticker in self.config.companies.index:
+            mm = pd.DataFrame(columns=[pn_cols.TICKER, pn_cols.MU_QTR_PE_RATIO, pn_cols.MU_TTM_PE_RATIO], index=['0 year', '1 year', '3 year', '5 year'])
+            mm.index.name = pn_cols.MU_NUM_YEARS
 
-        self.mu_multiples.loc['0 year'] = [self.multiples.loc[:, pn_cols.QTR_PE_RATIO].tail(1).mean() / 4, self.multiples.loc[:, pn_cols.TTM_QTR_PE_RATIO].tail(1).mean()]
-        self.mu_multiples.loc['1 year'] = np.nan if self.multiples.shape[0] < 4 else [self.multiples.loc[:, pn_cols.QTR_PE_RATIO].tail(4).mean() / 4, self.multiples.loc[:, pn_cols.TTM_QTR_PE_RATIO].tail(4).mean()]
-        self.mu_multiples.loc['3 year'] = np.nan if self.multiples.shape[0] < 12 else [self.multiples.loc[:, pn_cols.QTR_PE_RATIO].tail(12).mean() / 4, self.multiples.loc[:, pn_cols.TTM_QTR_PE_RATIO].tail(12).mean()]
-        self.mu_multiples.loc['5 year'] = np.nan if self.multiples.shape[0] < 20 else [self.multiples.loc[:, pn_cols.QTR_PE_RATIO].tail(20).mean() / 4, self.multiples.loc[:, pn_cols.TTM_QTR_PE_RATIO].tail(20).mean()]
+            mm.loc[('0 year'), [pn_cols.MU_QTR_PE_RATIO, pn_cols.MU_TTM_PE_RATIO]] = [self.multiples.loc[(ticker,), pn_cols.QTR_PE_RATIO].tail(1).mean() / 4, self.multiples.loc[(ticker,), pn_cols.TTM_PE_RATIO].tail(1).mean()]
+            mm.loc[('1 year'), [pn_cols.MU_QTR_PE_RATIO, pn_cols.MU_TTM_PE_RATIO]] = [self.multiples.loc[(ticker,), pn_cols.QTR_PE_RATIO].tail(4).mean() / 4, self.multiples.loc[(ticker,), pn_cols.TTM_PE_RATIO].tail(4).mean()]
+            mm.loc[('3 year'), [pn_cols.MU_QTR_PE_RATIO, pn_cols.MU_TTM_PE_RATIO]] = [self.multiples.loc[(ticker,), pn_cols.QTR_PE_RATIO].tail(12).mean() / 4, self.multiples.loc[(ticker,), pn_cols.TTM_PE_RATIO].tail(12).mean()]
+            mm.loc[('5 year'), [pn_cols.MU_QTR_PE_RATIO, pn_cols.MU_TTM_PE_RATIO]] = [self.multiples.loc[(ticker,), pn_cols.QTR_PE_RATIO].tail(20).mean() / 4, self.multiples.loc[(ticker,), pn_cols.TTM_PE_RATIO].tail(20).mean()]
+
+            mm.loc[:, pn_cols.TICKER] = ticker
+
+            self.mu_multiples = mm if self.mu_multiples is None else pd.concat([self.mu_multiples, mm])
+
+        self.mu_multiples.reset_index(inplace=True)
+        self.mu_multiples.set_index([pn_cols.TICKER, pn_cols.MU_NUM_YEARS], inplace=True)
+        self.mu_multiples.sort_index(inplace=True)
 
     # TODO
     def calc_present_fv(self):
