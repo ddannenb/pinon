28003bd8985895a704af1b0d3a62dd3118c1a9b9
daved <daved@audiblelogic.com>
2024-10-17 10:56:55 -0600

    Add sqlite db connections, CRUD methods.

--- pinon/configX.py
+++ pinon/configX.py
@@ -2,9 +2,25 @@ import sqlite3
 import pathlib
 from os import environ as env
 
+from pandas.io.sql import table_exists
+
+import configX
+
 PROJ_PATH = pathlib.Path(__file__).parent.parent
 SQLITE_DB_PATH = PROJ_PATH / "app_data"
 SQLITE_DB_FILE = "pinon_db.sqlite"
+
+USER_SCHEMA = {
+    0: 'user',
+    'name': 'TEXT'
+}
+
+PEER_GROUP_SCHEMA = {
+    0: 'peer_group',
+    'user_id': 'INTEGER',
+    'name': 'TEXT',
+    'past_years_requested': 'INTEGER'
+}
 
 env['SQLITE_DB_PATH'] = SQLITE_DB_PATH.absolute().as_posix()
 env['SQLITE_DB_FILE'] = SQLITE_DB_FILE
@@ -14,4 +30,111 @@ class ConfigX:
     def __init__(self):
         self.sqlite_db_file_path = env['SQLITE_DB_PATH'] + '/' + env['SQLITE_DB_FILE']
         self.sql_connection = sqlite3.connect(self.sqlite_db_file_path)
-        print(f"Changes: {self.sql_connection.total_changes}")
+        self.setup_db()
+
+    def setup_db(self):
+        cursor = self.sql_connection.cursor()
+        # cursor.execute("CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)")
+        cursor.execute("CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)")
+        cursor.execute("CREATE TABLE IF NOT EXISTS peer_group (id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, name TEXT, past_years_requested INTEGER)")
+        cursor.execute("CREATE TABLE IF NOT EXISTS ticker (id INTEGER PRIMARY KEY AUTOINCREMENT, peer_group_id INTEGER, ticker TEXT, evaluate BOOLEAN, peer_group_weight INTEGER)")
+        self.sql_connection.commit()
+
+    def create_user(self, name):
+        return self.create(USER_SCHEMA, {'name':name})
+        # cursor = self.sql_connection.cursor()
+        # sql = f"INSERT into user(name) VALUES ('{name}') RETURNING id"
+        # cursor.execute(sql)
+        # res = cursor.fetchone()[0]
+        # self.sql_connection.commit()
+        # return res
+
+    def read_user(self, id):
+        return self.read(USER_SCHEMA, id)
+        # cursor = self.sql_connection.cursor()
+        # sql = f"SELECT name FROM user WHERE id={id};"
+        # cursor.execute(sql)
+        # res = cursor.fetchone()
+        # if res is None:
+        #     return None
+        # else:
+        #     return {'name': res[0]}
+
+    def delete_user(self, id):
+        cursor = self.sql_connection.cursor()
+        sql = f"DELETE FROM user WHERE id={id};"
+        cursor.execute(sql)
+        self.sql_connection.commit()
+        return cursor.rowcount
+
+    def update_user(self, id, name):
+        return self.update(USER_SCHEMA, {'name': name})
+        # cursor = self.sql_connection.cursor()
+        # sql = f"UPDATE user SET name='{name}' WHERE id={id};"
+        # cursor.execute(sql)
+        # self.sql_connection.commit()
+        # return cursor.rowcount
+
+    def create_peer_group(self, user_id, name, past_years_requested):
+        return self.create(configX.PEER_GROUP_SCHEMA, {'user_id': user_id, 'name': name, 'past_years_requested': past_years_requested})
+        # cursor = self.sql_connection.cursor()
+        # sql = f"INSERT into peer_group(user_id, name, past_years_requested) VALUES ('{user_id}', '{name}', '{past_years_requested}') RETURNING id"
+        # cursor.execute(sql)
+        # res = cursor.fetchone()[0]
+        # self.sql_connection.commit()
+        # return res
+
+    def read_peer_group(self, id):
+        return self.read(configX.PEER_GROUP_SCHEMA, id)
+        # cursor = self.sql_connection.cursor()
+        # sql = f"SELECT name, past_years_requested FROM peer_group WHERE id={id};"
+        # cursor.execute(sql)
+        # res = cursor.fetchone()
+        # if res is None:
+        #     return None
+        # else:
+        #     return {'name': res[0], 'past_years_requested': res[1]}
+
+    def resolve_type(self, type, val):
+        match type:
+            case 'TEXT':
+                return f"'{val}'"
+            case 'INTEGER':
+                return f"{val}"
+            case 'BOOL':
+                return 0 if val else 1
+            case _:
+                return f"{val}"
+
+    def create(self, schema, values):
+        cursor = self.sql_connection.cursor()
+        table_name = schema[0]
+        cols = ', '.join([c for c in values.keys()])
+        vals = ', '.join([self.resolve_type(schema[k], values[k]) for k in values.keys()])
+        sql = f"INSERT INTO {table_name}({cols}) VALUES ({vals}) RETURNING id"
+        cursor.execute(sql)
+        res = cursor.fetchone()[0]
+        self.sql_connection.commit()
+        return res
+
+    def read(self, schema, id):
+        cursor = self.sql_connection.cursor()
+        table_name = schema[0]
+        cols_l = [c for c in list(schema.keys())[1:]]
+        cols = ', '.join(cols_l)
+        sql = f"SELECT {cols} FROM {table_name} WHERE id = {id}"
+        cursor.execute(sql)
+        res = cursor.fetchone()
+        if res is None:
+            return None
+        else:
+            return {k: v for (k, v) in zip(cols_l, res)}
+
+    def update(self, schema, id, values):
+        cursor = self.sql_connection.cursor()
+        table_name = schema[0]
+        sets_l = [f"{k} = {self.resolve_type(schema[k], values[k])}" for k in values.keys()]
+        sets = ', '.join(sets_l)
+        sql = f"UPDATE {table_name} SET {sets} WHERE id = {id}"
+        print('Break')
+
--- tests/test_pinon_configX.py
+++ tests/test_pinon_configX.py
@@ -1,11 +1,66 @@
 import unittest
+import pathlib
+from os import environ as env
+
 import pinon as pn
+import configX
+
+TEST_PATH = pathlib.Path(__file__).parent
+TEST_DATA_PATH = TEST_PATH / 'test_data'
+
+env['SQLITE_DB_PATH'] = TEST_DATA_PATH.absolute().as_posix()
+# env['SQLITE_DB_FILE'] = SQLITE_DB_FILE
 
 
 class TestPinonConfigX(unittest.TestCase):
-    def test_create_connection(self):
+    def test_setup_db(self):
         cx = pn.ConfigX()
-        print('Here')
+        cursor = cx.sql_connection.cursor()
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        sql_res = cursor.fetchall()
+        tables = [item[0] for item in sql_res]
+        self.assertIn('user', tables)
+        self.assertIn('peer_group', tables)
+        self.assertIn('ticker', tables)
+
+    def test_crud_user(self):
+        cx = pn.ConfigX()
+        user_name = 'Test User'
+        new_user_name = 'New User Name'
+        user_id = cx.create_user(user_name)
+        read_user = cx.read_user(user_id)
+        self.assertEqual(user_name, read_user['name'])
+
+        num_up = cx.update_user(user_id, new_user_name)
+        self.assertEqual(1, num_up)
+        up_user = cx.read_user(user_id)
+        self.assertEqual(new_user_name, up_user['name'])
+
+        num_del = cx.delete_user(user_id)
+        self.assertEqual(1, num_del)
+
+        no_user = cx.read_user(user_id)
+        self.assertIsNone(no_user)
+
+    def test_crud_peer_group(self):
+        cx = pn.ConfigX()
+        peer_group_name = 'Peer group name'
+        num_years_requested = 22
+        user_name = 'Test Linked User'
+        user_id = cx.create_user(user_name)
+        peer_group_id = cx.create_peer_group(user_id, peer_group_name, num_years_requested)
+        read_peer_group = cx.read_peer_group(peer_group_id)
+
+        print('Break')
+
+    def test_create(self):
+        cx = pn.ConfigX()
+        # cx.create('peer_group', {'user_id': 'A User Id', 'name': 'A Name', 'past_year_requested': 34})
+        cx.create(configX.PEER_GROUP_SCHEMA, {'user_id': 111, 'name': 'A Name', 'past_years_requested': 34})
+        print('Break')
+
+
+
 
 
 if __name__ == '__main__':
